4fe34f349f470c2f99519b47c06b074c6d2a16f6
OPOII <bryancamili198@hotmail.com>
2023-08-24 17:26:35 -0500

    continuación del proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ const routes: Routes = [
     path: 'products', loadChildren: () => import('./products/products.module').then(m => m.ProductsModule)
   },
   { path: 'products', loadChildren: () => import('./shared/shared.module').then(m => m.SharedModule) },
+  { path: 'signals', loadChildren: () => import('./signals/signals.module').then(m => m.SignalsModule) },
   {
     path: '**', redirectTo:'products'
   },
