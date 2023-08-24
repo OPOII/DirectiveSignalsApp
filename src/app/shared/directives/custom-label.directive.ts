fe889192447d3c9e1329450bc98cdc01d96916e0
OPOII <bryancamili198@hotmail.com>
2023-08-24 16:51:18 -0500

    poner todas las validaciones

--- src/app/shared/directives/custom-label.directive.ts
+++ src/app/shared/directives/custom-label.directive.ts
@@ -46,6 +46,17 @@ export class CustomLabelDirective implements OnInit {
 
     if(errors.includes('required')){
       this.htmlElement.nativeElement.innerText='Este campo es requerido';
+      return;
+    }
+    if(errors.includes('minlength')){
+      const min=this._errors!['minlength']['requiredLength'];
+      const current=this._errors!['minlength']['actualLength'];
+      this.htmlElement.nativeElement.innerText=`Minimo ${current}/${min} caracteres.`;
+      return;
+    }
+    if(errors.includes('email')){
+      this.htmlElement.nativeElement.innerText='No tiene formato de correo';
+      return;
     }
   }
 }
